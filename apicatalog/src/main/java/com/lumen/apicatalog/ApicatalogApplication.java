2f22900ccd6f1cd8a2b5fe4a59f1731985fcc3bf
Mishra <Manoranjan.Mishra@centurylink.com>
2021-09-24 21:55:45 +0530

    initial commit
    initial commit

---
+++ apicatalog/src/main/java/com/lumen/apicatalog/ApicatalogApplication.java
@@ -0,0 +1,13 @@
+package com.lumen.apicatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApicatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApicatalogApplication.class, args);
+	}
+
+}
---
+++ apicatalog/src/test/java/com/lumen/apicatalog/ApicatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lumen.apicatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApicatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
